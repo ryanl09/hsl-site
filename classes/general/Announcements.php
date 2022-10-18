a46cb4588278239031c79d63ecc4423d21b17ed7
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2022-10-18 16:40:42 -0400

    added Announcements.php

---
+++ classes/general/Announcements.php
@@ -0,0 +1,41 @@
+<?php
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    require_once($path . '/classes/util/tecdb.php');
+    require_once($path . '/classes/util/Sessions.php');
+
+    class Announcements {
+        private function __construct() {
+
+        }
+
+        static function get_all() {
+            $db = new tecdb();
+
+            $query = 'SELECT a.user_id, a.title, a.body, a.time
+                    FROM annoucements a';
+
+            $res = $db->query($query)->fetchAll();
+            
+            return $res;
+        }
+
+        static function create($title, $body) {
+            $id = 0;
+            if (!isset($_SESSION['user'])) {
+                return false;
+            }
+            $id = $_SESSION['user']->get_id();
+
+            $db = new tecdb();
+
+            $query = 'INSERT INTO announcements
+                        (`user_id`, title, body)
+                        VALUES (?, ?, ?)';
+
+            $res = $db->query($query, $id, $title, $body)->lastInsertID();
+            return $res;
+        }
+
+        
+    }
+?>
