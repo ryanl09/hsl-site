00ab398fe12c27c8d48edaeb0515ea751e8180cd
ryanl09 <71403615+ryanl09@users.noreply.github.com>
2022-11-03 16:02:17 -0400

    login-ajax
    di

--- ajax/login-ajax.php
+++ ajax/login-ajax.php
@@ -2,64 +2,16 @@
 
 $path = $_SERVER['DOCUMENT_ROOT'];
 require_once($path . '/classes/services/LoginService.php');
-include_once($path . '/classes/util/ajaxerror.php');
 require_once($path . '/classes/util/Sessions.php');
 
-if ((isset($_SERVER['HTTP_X_REQUESTED_WITH'])) && ($_SERVER['HTTP_X_REQUESTED_WITH']==='XMLHttpRequest')) {
-    if (!$_SERVER['REQUEST_METHOD']==='POST') {
-        echo 'Invalid request';
-        die();
-    }
-
-    /*
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        $address = 'http://' . $_SERVER['SERVER_NAME'];
-        if (strpos($address, $_SERVER['HTTP_ORIGIN']) !== 0) {
-            echo json_encode(
-                array(
-                    'error' => 'Invalid origin header: ' . $_SERVER['HTTP_ORIGIN']
-                );
-            );
-            die();
-        }
-    } else {
-        echo json_encode(
-            array(
-                'error' => 'Missing origin header.'
-            )
-        );
-        die();
-    }
-    */
-    
-    if (isset($_POST['csrf'])) {
-        if ($_POST['csrf']!==$_SESSION['csrf']){
-            echo json_encode(
-                array(
-                    'status' => 0,
-                    'errors' => ['Invalid CSRF token']
-                )
-            );
-            die();
-        }
-    } else {
-        echo json_encode(
-            array(
-                'status' => 0,
-                'errors' => ['Missing CSRF token']
-            )
-        );
-        die();
-    }
-    
-    $ls = new LoginService($_POST);
-    $login = $ls->login();
-
-    echo json_encode($login);
-
-
-} else {
-    echo 'Access denied';
+if (!$_SERVER['REQUEST_METHOD']==='POST') {
+    echo 'Invalid request';
+    die();
 }
+
+$ls = new LoginService($db, $_POST);
+$login = $ls->login();
+
+echo json_encode($login);
 
 ?>
