9ffb16a9c01fb540e3aa6dd6cf38f4f525984046
ryanl09 <71403615+ryanl09@users.noreply.github.com>
2022-11-03 16:03:52 -0400

    reg and login di - sessions
    removed includes

--- ajax/login-ajax.php
+++ ajax/login-ajax.php
@@ -2,7 +2,6 @@
 
 $path = $_SERVER['DOCUMENT_ROOT'];
 require_once($path . '/classes/services/LoginService.php');
-require_once($path . '/classes/util/Sessions.php');
 
 if (!$_SERVER['REQUEST_METHOD']==='POST') {
     echo 'Invalid request';
--- ajax/reg-ajax.php
+++ ajax/reg-ajax.php
@@ -4,10 +4,6 @@
 include_once('ajax-util.php');
 
 $path = $_SERVER['DOCUMENT_ROOT'];
-include_once($path . '/classes/security/csrf.php');
-include_once($path . '/classes/util/ajaxerror.php');
-include_once($path . '/classes/util/tecdb.php');
-require_once($path . '/classes/util/Sessions.php');
 
 if (!isset($_SESSION['user'])){
     echo ajaxerror::e('errors', ['No user account found']);
@@ -15,18 +11,6 @@ if (!isset($_SESSION['user'])){
 }
 
 $user = $_SESSION['user'];
-
-$post = check_post();
-if (!$post['status']) {
-    echo ajaxerror::e('errors', [$post['error']]);
-    die();
-}
-
-$csrf = CSRF::post();
-if (!$csrf) {
-    echo ajaxerror::e('errors', ['Invalid CSRF token']);
-    die();
-}
 
 /*
 
@@ -46,7 +30,6 @@ if (isset($_POST['update_team'])){
         "UPDATE `users`
         SET `team_id` = ?
         WHERE `user_id` = ?";
-        $db = new tecdb();
 
         $res = $db->query($query, $_POST['update_team'], $_SESSION['user']->get_id())->affectedRows();
         echo json_encode(
