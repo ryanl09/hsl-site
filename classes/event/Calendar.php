f12025e82b5e8e8f36d70eb978c0cc2313bef7c3
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2022-11-10 17:59:35 -0500

    working on populating calendar with matches

---
+++ classes/event/Calendar.php
@@ -0,0 +1,100 @@
+<?php
+class Calendar {
+
+    private $active_year, $active_month, $active_day;
+    private $events = [];
+
+    public function __construct($date = null) {
+        $this->active_year = $date != null ? date('Y', strtotime($date)) : date('Y');
+        $this->active_month = $date != null ? date('m', strtotime($date)) : date('m');
+        $this->active_day = $date != null ? date('d', strtotime($date)) : date('d');
+    }
+
+    static function get_all_events($db) {
+        $query = 
+        "SELECT t.team_name AS home, t2.team_name AS away, events.event_time, events.event_date, s.division, events.id, s.tag as home_tag, s2.tag as away_tag,
+        s.id as h_id, s2.id as a_id
+        FROM events
+        INNER JOIN subteams s
+            ON s.id = events.event_home
+        INNER JOIN teams t
+            ON s.team_id = t.id
+        INNER JOIN subteams s2
+            ON s2.id = events.event_away
+        INNER JOIN teams t2
+            ON s2.team_id = t2.id
+        ORDER BY events.event_time ASC";
+
+        $res = $db->query($query)->fetchAll();
+        return $res;
+    }
+
+    public function add_event($txt, $date, $days = 1, $color = '') {
+        $color = $color ? ' ' . $color : $color;
+        $this->events[] = [$txt, $date, $days, $color];
+    }   
+
+    public function show() {
+        $num_days = date('t', strtotime($this->active_day . '-' . $this->active_month . '-' . $this->active_year));
+        $num_days_last_month = date('j', strtotime('last day of previous month', strtotime($this->active_day . '-' . $this->active_month . '-' . $this->active_year)));
+        $days = [0 => 'Sun', 1 => 'Mon', 2 => 'Tue', 3 => 'Wed', 4 => 'Thu', 5 => 'Fri', 6 => 'Sat'];
+        $first_day_of_week = array_search(date('D', strtotime($this->active_year . '-' . $this->active_month . '-1')), $days);
+        $html = '<div class="calendar">';
+        $html .= '<div class="header">';
+        $html .= '<div class="month-year">';
+        $html .= date('F Y', strtotime($this->active_year . '-' . $this->active_month . '-' . $this->active_day));
+        $html .= '</div>';
+        $html .= '</div>';
+        $html .= '<div class="days">';
+        foreach ($days as $day) {
+            $html .= '
+                <div class="day_name">
+                    ' . $day . '
+                </div>
+            ';
+        }
+        for ($i = $first_day_of_week; $i > 0; $i--) {
+            $c = '';
+            $html .= '
+                <div class="day_num ignore'.$c.'">
+                    ' . ($num_days_last_month-$i+1) . '
+                </div>
+            ';
+        }
+        for ($i = 1; $i <= $num_days; $i++) {
+            $selected = '';
+            if ($i == $this->active_day) {
+                $selected = ' selected';
+            }
+            $html .= '<div class="day_num' . $selected . '">';
+            $html .= '<span>' . $i . '</span>';
+            foreach ($this->events as $event) {
+                for ($d = 0; $d <= ($event[2]-1); $d++) {
+                    if (date('y-m-d', strtotime($this->active_year . '-' . $this->active_month . '-' . $i . ' -' . $d . ' day')) == date('y-m-d', strtotime($event[1]))) {
+                        $html .= '<div class="event' . $event[3] . '">';
+                        $html .= $event[0];
+                        $html .= '</div>';
+                    }
+                }
+            }
+            $html .= '</div>';
+        }
+        $chris  = (42-$num_days-max($first_day_of_week, 0));
+        $can=true;
+        for ($i = 1; $i <= $chris; $i++) {
+            $c='';
+            if ($chris-$i < 7&&$can){
+                $c=' b-left';
+                $can=false; 
+            }
+            $html .= '
+                <div class="day_num ignore'.$c.'">
+                    ' . $i . '
+                </div>
+            ';
+        }
+        $html .= '</div>';
+        $html .= '</div>';
+        return $html;
+    }
+}?>
--- views/events.php
+++ views/events.php
@@ -29,95 +29,6 @@ foreach ($games as $i => $row){
 }
 $game_icons.='</div>';
 $game_icons2.='</div>';
-
-class Calendar {
-
-    private $active_year, $active_month, $active_day;
-    private $events = [];
-
-    public function __construct($date = null) {
-        $this->active_year = $date != null ? date('Y', strtotime($date)) : date('Y');
-        $this->active_month = $date != null ? date('m', strtotime($date)) : date('m');
-        $this->active_day = $date != null ? date('d', strtotime($date)) : date('d');
-        add_all_events_for_month();
-    }
-
-    public function add_all_events_for_month() {
-        $events_for_month = Game::get_all_games_for_month($db, date());
-        foreach($games as $i => $row) {
-            
-        }
-    }
-
-    public function add_event($txt, $date, $days = 1, $color = '') {
-        $color = $color ? ' ' . $color : $color;
-        $this->events[] = [$txt, $date, $days, $color];
-    }
-
-    public function show() {
-        $num_days = date('t', strtotime($this->active_day . '-' . $this->active_month . '-' . $this->active_year));
-        $num_days_last_month = date('j', strtotime('last day of previous month', strtotime($this->active_day . '-' . $this->active_month . '-' . $this->active_year)));
-        $days = [0 => 'Sun', 1 => 'Mon', 2 => 'Tue', 3 => 'Wed', 4 => 'Thu', 5 => 'Fri', 6 => 'Sat'];
-        $first_day_of_week = array_search(date('D', strtotime($this->active_year . '-' . $this->active_month . '-1')), $days);
-        $html = '<div class="calendar">';
-        $html .= '<div class="header">';
-        $html .= '<div class="month-year">';
-        $html .= date('F Y', strtotime($this->active_year . '-' . $this->active_month . '-' . $this->active_day));
-        $html .= '</div>';
-        $html .= '</div>';
-        $html .= '<div class="days">';
-        foreach ($days as $day) {
-            $html .= '
-                <div class="day_name">
-                    ' . $day . '
-                </div>
-            ';
-        }
-        for ($i = $first_day_of_week; $i > 0; $i--) {
-            $c = '';
-            $html .= '
-                <div class="day_num ignore'.$c.'">
-                    ' . ($num_days_last_month-$i+1) . '
-                </div>
-            ';
-        }
-        for ($i = 1; $i <= $num_days; $i++) {
-            $selected = '';
-            if ($i == $this->active_day) {
-                $selected = ' selected';
-            }
-            $html .= '<div class="day_num' . $selected . '">';
-            $html .= '<span>' . $i . '</span>';
-            foreach ($this->events as $event) {
-                for ($d = 0; $d <= ($event[2]-1); $d++) {
-                    if (date('y-m-d', strtotime($this->active_year . '-' . $this->active_month . '-' . $i . ' -' . $d . ' day')) == date('y-m-d', strtotime($event[1]))) {
-                        $html .= '<div class="event' . $event[3] . '">';
-                        $html .= $event[0];
-                        $html .= '</div>';
-                    }
-                }
-            }
-            $html .= '</div>';
-        }
-        $chris  = (42-$num_days-max($first_day_of_week, 0));
-        $can=true;
-        for ($i = 1; $i <= $chris; $i++) {
-            $c='';
-            if ($chris-$i < 7&&$can){
-                $c=' b-left';
-                $can=false; 
-            }
-            $html .= '
-                <div class="day_num ignore'.$c.'">
-                    ' . $i . '
-                </div>
-            ';
-        }
-        $html .= '</div>';
-        $html .= '</div>';
-        return $html;
-    }
-}
 ?>
 
 <div class="top-section">
@@ -226,10 +137,16 @@ class Calendar {
     </div>
 </div>
 <div class="mid-section2" style="display: none">
-    <?php $calendar = new Calendar(); ?>
+    <?php 
+        require_once($path . '/classes/event/Calendar.php');
+        $calendar = new Calendar();
+    ?>
     <body>
         <div class="content">
-            <?php echo $calendar->show();?>
+            <?php
+                $calendar->add_event("Test Game", "November 12 2022");
+                echo $calendar->show();
+            ?>
         </div>
     </body>
 </div>
