896ad40e9a26906461e448737d8afe741915cb0c
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2022-11-22 11:37:57 -0500

    clickable banner to notify unverified accts

--- views/activate.php
+++ views/activate.php
@@ -22,7 +22,7 @@
             <h1>Your account has been activated!</h1>
             <p><a href="https://tecesports.com/login">Click here to log in</a></p>
         </div>
-    <?php } else { ?>
+        <?php } else { ?>
         <div class="activate-header">
             <h1>Your account is either already activated or does not exist :(</h1>
             <p><a href="https://tecesports.com/register">Click here to create an account</a></p>
